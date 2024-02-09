b1366920cf7f79b15ebad7501881fc6d5e60dcfb
sahadot001s <sahadot001s@gmail.com>
2024-02-09 15:10:09 +0600

    ff

---
+++ input.js
@@ -0,0 +1,133 @@
+{
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        autoplay: {
+          delay: 1500,
+        },
+        breakpoints: {
+          1200: {
+              slidesPerView: 4,
+              spaceBetweenSlides: 0
+          },
+          992: {
+              slidesPerView: 3,
+              spaceBetweenSlides: 20
+          },
+          668: {
+            slidesPerView: 2,
+            spaceBetweenSlides: 20
+        },
+        0: {
+          slidesPerView: 1,
+          spaceBetweenSlides: 20
+      }
+      }
+      });
+}
+
+{
+      var swiper = new Swiper(".mySwiper1", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        autoplay: {
+          delay: 1500,
+        },
+      });
+}
+
+{
+    var swiper = new Swiper(".slid3i", {
+        slidesPerView: 6,
+        spaceBetween: 10,
+        autoplay: {
+          delay: 1500,
+        },
+        breakpoints: {
+          1920: {
+            slidesPerView: 6,
+            spaceBetweenSlides: 0
+        },
+          1400: {
+              slidesPerView: 5,
+              spaceBetweenSlides: 0
+          },
+          992: {
+              slidesPerView: 4,
+              spaceBetweenSlides: 20
+          },
+          668: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 20
+        },
+        0: {
+          slidesPerView: 1,
+          spaceBetweenSlides: 20
+      }
+      }
+      });
+}
+
+{
+  var swiper = new Swiper(".mySwiper6", {
+      slidesPerView: 6,
+      spaceBetween: 10,
+      autoplay: {
+        delay: 1500,
+      },
+      breakpoints: {
+        1920: {
+          slidesPerView: 6,
+          spaceBetweenSlides: 0
+      },
+      1400: {
+        slidesPerView: 5,
+        spaceBetweenSlides: 0
+      },
+        1200: {
+            slidesPerView: 4,
+            spaceBetweenSlides: 0
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetweenSlides: 20
+        },
+        668: {
+          slidesPerView: 3,
+          spaceBetweenSlides: 20
+      },
+      0: {
+        slidesPerView: 1,
+        spaceBetweenSlides: 20
+    }
+    }
+    });
+}
+
+{
+  var swiper = new Swiper(".swiper3", {
+      slidesPerView: 4,
+      spaceBetween: 10,
+      autoplay: {
+        delay: 1500,
+      },
+      breakpoints: {
+        1200: {
+            slidesPerView: 4,
+            spaceBetweenSlides: 0
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 20
+        },
+        668: {
+          slidesPerView: 2,
+          spaceBetweenSlides: 20
+      },
+      0: {
+        slidesPerView: 1,
+        spaceBetweenSlides: 20
+    }
+    }
+    });
+  }
